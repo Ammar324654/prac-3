49e27c598fe143ec5cf27177b4beba5795bee42e
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-23 14:58:32 +1030

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,34 +1,17 @@
+#include "Truckloads.h"
 #include <iostream>
 
-class Truckloads {
-public:
-    int numTrucks(int numCrates, int loadSize) {
-        // Base case: If the number of crates is less than or equal to the load size, one truck is needed
-        if (numCrates <= loadSize) {
-            return 1;
-        }
-        
-        // Recursive case: Divide crates in half and calculate trucks needed for each half
-        int halfCrates = numCrates / 2;
-        int trucks = 0;
-        
-        // For each half, calculate trucks and divide further if necessary
-        while (halfCrates > 0) {
-            trucks += numTrucks(halfCrates, loadSize);  // Call recursively for the divided pile
-            halfCrates = halfCrates / 2;  // Keep dividing further
-        }
-        
-        return trucks;
+Truckloads::Truckloads() {}
+
+Truckloads::~Truckloads() {}
+
+int Truckloads::numTrucks(int numCrates, int loadSize) {
+    if (numCrates <= loadSize) {
+        return 1;
     }
-};
 
-int main() {
-    Truckloads t;
-    
-    int numCrates = 14;
-    int loadSize = 3;
-    std::cout << "Number of trucks required: " << t.numTrucks(numCrates, loadSize) << std::endl;
-    
-    return 0;
+    int L = numCrates / 2;
+    int R = numCrates - L;
+
+    return numTrucks(L, loadSize) + numTrucks(R, loadSize);
 }
-
