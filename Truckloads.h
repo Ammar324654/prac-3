691a16a48917eca7476286f0badd22aa3e9941e6
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-23 14:38:56 +1030

    Update Truckloads.h

--- Truckloads.h
+++ Truckloads.h
@@ -1,32 +1,21 @@
-#ifndef TRUCKLOADS_H
-#define TRUCKLOADS_H
+#ifndef TRUCKLOAD_H
+#define TRUCKLOAD_H
 
-#include <memory>
 
-// Abstract class defining the truck loading strategy
-class TruckLoadStrategy {
+#include <iostream>
+#include <string>
+
+
+
+class Truckloads
+{
 public:
-    virtual int calculateTrucks(int numCrates, int loadSize) = 0;
-    virtual ~TruckLoadStrategy() = default;
-};
+    Truckloads();
+    ~Truckloads();
 
-// Recursive strategy for calculating truckloads
-class RecursiveTruckLoad : public TruckLoadStrategy {
-public:
-    int calculateTrucks(int numCrates, int loadSize) override;
-};
-
-// Class that calculates the truckloads using a given strategy
-class Truckloads {
-private:
-    std::shared_ptr<TruckLoadStrategy> strategy;
-
-public:
-    // Constructor takes a strategy
-    Truckloads(std::shared_ptr<TruckLoadStrategy> strat);
-
-    // Method to calculate the number of trucks
     int numTrucks(int numCrates, int loadSize);
 };
 
-#endif // TRUCKLOADS_H
+
+
+#endif  // TRUCKLOAD_H
