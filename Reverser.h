88af8fc98cc72fbf098d00c6c0b11543fa0acede
Ammar324654 <140363685+Ammar324654@users.noreply.github.com>
2025-03-23 14:38:28 +1030

    Update Reverser.h

--- Reverser.h
+++ Reverser.h
@@ -1,37 +1,27 @@
 #ifndef REVERSER_H
 #define REVERSER_H
 
+
+#include <iostream>
 #include <string>
 
-// Abstract class defining the reverse strategy
-template <typename T>
-class ReverseStrategy {
-public:
-    virtual T reverse(T input) = 0;
-    virtual ~ReverseStrategy() = default;
-};
+using namespace std;
 
-// Reverser for digits (integers)
-class ReverseDigit : public ReverseStrategy<int> {
-public:
-    int reverse(int value) override;
-private:
-    int reverseDigitHelper(int value, int reversed);
-};
-
-// Reverser for strings
-class ReverseString : public ReverseStrategy<std::string> {
-public:
-    std::string reverse(std::string characters) override;
-private:
-    std::string reverseStringHelper(std::string characters, int index);
-};
-
-// Reverser class that uses a reverse strategy
 class Reverser {
 public:
-    template <typename T>
-    T reverseInput(T input);
+    Reverser();
+    ~Reverser();
+
+    int reverseDigit(int value, int acc);
+    int reverseDigit(int value);
+
+    string reverseString(string characters, string acc);
+    string reverseString(string characters);
 };
 
-#endif // REVERSER_H
+
+
+
+
+
+#endif  // REVERSER_H
